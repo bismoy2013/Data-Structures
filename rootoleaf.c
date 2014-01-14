8a46b3b1bc1282b6204f82e08cdc89e382ead109
Bismoy Murasing <bmurasing@gmail.com>
2014-01-14 05:48:59 +0530

    Root to Leaf Nodes Traversing and sum of paths

---
+++ rootoleaf.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+   int data;
+   struct node* left;
+   struct node* right;
+};
+void printPathsRecur(struct node* node, int path[], int pathLen);
+void printArray(int ints[], int len);
+void printPaths(struct node* node)
+{
+  int path[1000];
+  printPathsRecur(node, path, 0);
+}
+void printPathsRecur(struct node* node, int path[], int pathLen)
+{
+  if (node==NULL)
+    return;
+  path[pathLen] = node->data;
+  pathLen++;
+  if (node->left==NULL && node->right==NULL)
+  {
+    printArray(path, pathLen);
+  }
+  else
+  {
+    printPathsRecur(node->left, path, pathLen);
+    printPathsRecur(node->right, path, pathLen);
+  }
+}
+void printArray(int ints[], int len)
+{
+  int i,sum=0;
+
+  for (i=0; i<len; i++)
+  {
+      sum+=ints[i];
+    printf("%d ", ints[i]);
+  }
+  printf("Sum: %d",sum);
+  printf("\n");
+}
+struct node* newnode(int data)
+{
+  struct node* node = (struct node*)malloc(sizeof(struct node));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+
+  return(node);
+}
+int main()
+{
+  struct node *root = newnode(10);
+  root->left        = newnode(8);
+  root->right       = newnode(2);
+  root->left->left  = newnode(3);
+  root->left->right = newnode(5);
+  root->right->left = newnode(2);
+
+  printPaths(root);
+
+  getchar();
+  return 0;
+}
